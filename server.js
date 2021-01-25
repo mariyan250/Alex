3d6821f04b6aa08abcae78fcc221eb23da775bd5
Krasimir250 <krasimir.kabakov18@gmail.com>
2021-01-25 09:53:44 +0200

    dobaveni ezici

--- server.js
+++ server.js
@@ -20,34 +20,44 @@ bot.on('message', async (event, chat) => {
   if (['hey', 'hi', 'hello'].includes(text.toLowerCase())) {
     await chat.sendMessage('Hello, sir!');
   } else if (
-    text.toLowerCase().includes('син цвят' || 'sin cvqt' || 'color blue')
+    text.toLowerCase().includes('син цвят') ||
+    text.toLowerCase().includes('sin cvqt') ||
+    text.toLowerCase().includes('color blue')
   ) {
     io.emit('color', 'blue');
   } else if (
-    text.toLowerCase().includes('червен цвят' || 'cherven cvqt' || 'color red')
+    text.toLowerCase().includes('червен цвят') ||
+    text.toLowerCase().includes('cherven cvqt') ||
+    text.toLowerCase().includes('color red')
   ) {
     io.emit('color', 'red');
   } else if (
-    text
-      .toLowerCase()
-      .includes('диско' || 'дискотека' || 'disko' || 'diskoteka')
+    text.toLowerCase().includes('диско') ||
+    text.toLowerCase().includes('дискотека') ||
+    text.toLowerCase().includes('diskoteka') ||
+    text.toLowerCase().includes('disko')
   ) {
     io.emit('color', 'disco');
   } else if (
-    text.toLowerCase().includes('стоп' || 'спри' || 'stop' || 'spri')
+    text.toLowerCase().includes('стоп') ||
+    text.toLowerCase().includes('спри') ||
+    text.toLowerCase().includes('stop')
   ) {
     io.emit('color', 'stop');
-  } else if (text.toLowerCase().includes('пусни' || 'pusni')) {
+  } else if (text.toLowerCase().includes('пусни')) {
     const music = text.toLowerCase().split('пусни')[1];
     YouTube.search(music, { limit: 1 })
       .then((res) => {
         io.emit('youtube link', res[0].id);
       })
       .catch((err) => console.log(err));
-  } else if (text.toLowerCase().includes('покажи' || 'pokaji')) {
+  } else if (text.toLowerCase().includes('покажи')) {
     io.emit('video controls', 'show');
   } else if (
-    text.toLowerCase().includes('скрии' || 'skrii' || 'skri')
+    text.toLowerCase().includes('скрии') ||
+    text.toLowerCase().includes('скрий') ||
+    text.toLowerCase().includes('skrii') ||
+    text.toLowerCase().includes('skri')
   ) {
     io.emit('video controls', 'hide');
   }
