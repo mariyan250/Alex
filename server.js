23020f9d416da51487f5b83c7cf0ae5309a68d07
Krasimir250 <krasimir.kabakov18@gmail.com>
2021-01-25 09:37:24 +0200

    smeneni vsichki cvetove

--- server.js
+++ server.js
@@ -19,26 +19,35 @@ bot.on('message', async (event, chat) => {
 
   if (['hey', 'hi', 'hello'].includes(text.toLowerCase())) {
     await chat.sendMessage('Hello, sir!');
-  } else if (text.toLowerCase().includes('син цвят')) {
+  } else if (
+    text.toLowerCase().includes('син цвят' || 'sin cvqt' || 'color blue')
+  ) {
     io.emit('color', 'blue');
-  } else if (text.toLowerCase().includes('червено')) {
+  } else if (
+    text.toLowerCase().includes('червен цвят' || 'cherven cvqt' || 'color red')
+  ) {
     io.emit('color', 'red');
-  } else if (text.toLowerCase().includes('дискотека')) {
+  } else if (
+    text
+      .toLowerCase()
+      .includes('диско' || 'дискотека' || 'disko' || 'diskoteka')
+  ) {
     io.emit('color', 'disco');
-  } else if (text.toLowerCase().includes('стоп')) {
+  } else if (
+    text.toLowerCase().includes('стоп' || 'спри' || 'stop' || 'spri')
+  ) {
     io.emit('color', 'stop');
-  } else if (text.toLowerCase().includes('пусни')) {
+  } else if (text.toLowerCase().includes('пусни' || 'pusni')) {
     const music = text.toLowerCase().split('пусни')[1];
     YouTube.search(music, { limit: 1 })
       .then((res) => {
         io.emit('youtube link', res[0].id);
       })
       .catch((err) => console.log(err));
-  } else if (text.toLowerCase().includes('покажи')) {
+  } else if (text.toLowerCase().includes('покажи' || 'pokaji')) {
     io.emit('video controls', 'show');
   } else if (
-    text.toLowerCase().includes('скрии') ||
-    text.toLowerCase().includes('скрий')
+    text.toLowerCase().includes('скрии' || 'skrii' || 'skri')
   ) {
     io.emit('video controls', 'hide');
   }
